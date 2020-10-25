8f71f9a61db59baf3ac9a6dec0e33135b8766f7b
huhufeng <1120077173@qq.com>
2020-10-26 00:56:57 +0800

    add

--- vue源码学习/utils.js
+++ vue源码学习/utils.js
@@ -112,6 +112,81 @@ export function toString(val) {
 
 export function toNumber(val) {
     const n = parseFloat(val)
-    return isNaN(n) ? val : n 
+    return isNaN(n) ? val : n
 }
 
+
+// 利用闭包实现一个缓存机制
+// 第二个参数可以实现是否区分大小写的 是一个boolean值
+
+export function makeMap(str, expectsLowerCase) {
+    const map = Object.create(null)
+    const list = str.split(',')
+
+    for(let i = 0; i < list.length; i++) {
+        map[list[i]] = true
+    }
+    return expectsLowerCase ? val => map[val.toLowerCase()] : val => map[val]
+}
+
+/**
+ * eg：
+ * let myFunc = makeMap('张三， 李四， 王麻子')
+ * myFunc('张三')
+ * return true
+ * */
+
+// 从数组中删除一项
+
+export function remove(arr, item) {
+    if (arr.length) {
+        const index = arr.indexOf(item)
+        if (index > -1) {
+            return arr.splice(index, 1)
+        }
+    }
+}
+
+// 判断一个属性是不是对象自身的属性而不会从原型链上去找
+const hasOwnProperty = Object.prototype.hasOwnProperty
+export function hasOwn(obj, key) {
+    return hasOwnProperty.call(obj, key)
+}
+
+// 创建一个纯函数的缓存版本
+// 加入我们有一个fn 函数是 function fn(str) { return str.toLoweCase() };
+// 后面我们会用一个变量来接收cached函数返回来的函数 每次将我们执行的值 缓存起来 这样可以极大的提高性能
+
+export function cached(fn) {
+    const cache = Object.create(null)
+    return (
+        function cachedFn(str) {
+            const hit = cache[str]
+            return hit || (cache[str] = fn(str))
+        }
+    )
+}
+
+
+// 驼峰
+const cameLizeRE = /-(\w)/g
+
+export const camelize = cached(str, () => {
+    return str.replace(cameLizeRE, (_, c) => c ? c.toUpperCase() : '')
+})
+
+// 将首字母大写
+export const capitalize = cached(str,() => {
+    return str.charAt(0).toUpperCase() + str.slice(1)
+})
+
+// 大写转连字符
+
+const hyphenateRE = /\B([A-Z])/g
+export const hyphenate = cached(str, () => {
+  return str.replace(hyphenateRE, '-$1').toLowerCase()
+})
+
+// 什么也不干
+
+export function noop (a, b, c) {}
